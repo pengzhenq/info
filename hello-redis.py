e10af6a8bc425396402669261f78f0c7cdae0498
pengzhenqiu <2602678119@qq.com>
2022-06-08 15:22:56 +0800

    test

---
+++ hello-redis.py
@@ -0,0 +1,8 @@
+from redis import Redis
+
+redis_cli = Redis(host="192.168.139.131",port=6379,db=0)
+
+# redis_cli.set('qqzzq','qqzzq')
+# res = redis_cli.get('qqzzq')
+# print(res)
+redis_cli.delete('qqzzq')
